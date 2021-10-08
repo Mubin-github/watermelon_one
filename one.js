3d36da6fd05bc56b9c22ea38cb36383330bdfc18
Mubin-github <hasibulislam111997@gmail.com>
2021-10-08 21:12:05 +0600

    new change

--- one.js
+++ one.js
@@ -1,2 +1,4 @@
-const input = prompt("What's your name?");
-alert(`Your name is ${input}`);
+// const input = prompt("What's your name?");
+// alert(`Your name is ${input}`);
+var name = prompt("enter your name");
+console.log("hello" + name + "!");
